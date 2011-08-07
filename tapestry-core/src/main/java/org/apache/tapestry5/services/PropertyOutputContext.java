e208f0d598cefb7e5f65506cbb84f908dc96aae9
Dragan Sahpaski <dragan.sahpaskix@gmail.com>
2011-08-07 12:56:18 +0200

    Removing ontextmenu's dependency on PropertyOutputContext. Solved it thanks to the EmbeddedMixin concept

--- tapestry-core/src/main/java/org/apache/tapestry5/services/PropertyOutputContext.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/services/PropertyOutputContext.java
@@ -18,29 +18,27 @@ import org.apache.tapestry5.corelib.components.Grid;
 import org.apache.tapestry5.ioc.Messages;
 
 /**
- * Provides context information needed when displaying a value. This interface is an integral part of the {@link Grid} and similar output
- * components. It is made available to components via an {@link org.apache.tapestry5.annotations.Environmental} annotation.
+ * Provides context information needed when displaying a value. This interface is an integral part of the {@link Grid}
+ * and similar output components.    It is made available to components via an {@link
+ * org.apache.tapestry5.annotations.Environmental} annotation.
  */
 public interface PropertyOutputContext
 {
-    /**
-     * Returns the value of the object (the object being displayed is encapsulated by the context).
-     */
-    Object getObjectValue();
-
     /**
      * Returns the value of the property (the object being displayed is encapsulated by the context).
      */
     Object getPropertyValue();
 
     /**
-     * Returns the message catalog appropriate for use. In practice, this is the message catalog of the container of the {@link Grid}
-     * component. This is used, for example, to locate labels for fields, or to locate string representations of Enums.
+     * Returns the message catalog appropriate for use. In practice, this is the message catalog of the container of the
+     * {@link Grid} component. This is used, for example, to locate labels for fields, or to locate string
+     * representations of Enums.
      */
     Messages getMessages();
 
     /**
-     * Returns a string that identifies the property, usually the property name. This is used as the basis for the client-side client id.
+     * Returns a string that identifies the property, usually the property name. This is used as the basis for the
+     * client-side client id.
      */
     String getPropertyId();
 
